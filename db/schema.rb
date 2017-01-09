c60044b7b285a75fea2730e8dbadf9217fe6afa7
the-undefined <joe@joejames.io>
2017-01-09 10:52:55 +0000

    [HOOZ-29] CRUD Rooms and Finishes
    - FIX: can upload a picture for each finish
    - Introduce :image_preview simple form input

--- db/schema.rb
+++ db/schema.rb
@@ -234,6 +234,8 @@ ActiveRecord::Schema.define(version: 20170105142623) do
     t.integer  "developer_id"
     t.integer  "division_id"
     t.integer  "development_id"
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_plots_on_deleted_at", using: :btree
     t.index ["developer_id"], name: "index_plots_on_developer_id", using: :btree
     t.index ["development_id"], name: "index_plots_on_development_id", using: :btree
     t.index ["division_id"], name: "index_plots_on_division_id", using: :btree
