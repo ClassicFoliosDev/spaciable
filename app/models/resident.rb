1d1b6948242ca5f93dc64af910629c98466e3343
the-undefined <joe@joejames.io>
2017-03-02 15:53:52 +0000

    Residents cannot register themselves

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -6,7 +6,7 @@ class Resident < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :resident_notifications
