0a7cfb0e55667355755926dba4ed0d08856259e9
Jamie Piper <jamiethepiper@users.noreply.github.com>
2017-03-13 10:12:24 +0000

    responsive appliances homeowner view (#178)

--- features/steps/homeowner/appliances_steps.rb
+++ features/steps/homeowner/appliances_steps.rb
@@ -22,12 +22,12 @@ Then(/^I should see the appliances for my plot$/) do
     expect(page).to have_content(CreateFixture.appliance_manufacturer_name)
   end
 
-  within ".register" do
+  within ".appliance" do
     register_appliance = page.find("a")
     expect(register_appliance[:href]).to have_content("www.bosch-home.co.uk/register-your-appliance.html")
   end
 
-  within ".e-rating" do
+  within ".details" do
     image = page.find("img")
 
     expect(image["src"]).to have_content("Hoozzi Energy Rating A1")
