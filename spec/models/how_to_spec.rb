2a0e34ac969bf04b684a9b115520f57df5d6cd1a
Joe <joe.james@alliants.com>
2017-04-06 10:08:57 +0100

    Add missing spec for HowTo validations (#247)

---
+++ spec/models/how_to_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe HowTo do
+  describe "featured" do
+    it "should not allow multiple how tos to share the same featured position" do
+      create(:how_to, featured: 1)
+      second_how_to = build(:how_to, featured: 1)
+
+      second_how_to.validate
+
+      error = I18n.t("activerecord.errors.messages.taken")
+      expect(second_how_to.errors[:featured]).to include(error)
+    end
+
+    it "should allow multiple non-featured how tos" do
+      create(:how_to, featured: nil)
+      second_how_to = build(:how_to, featured: nil)
+
+      second_how_to.validate
+
+      error = I18n.t("activerecord.errors.messages.taken")
+      expect(second_how_to.errors[:featured]).not_to include(error)
+    end
+  end
+end
