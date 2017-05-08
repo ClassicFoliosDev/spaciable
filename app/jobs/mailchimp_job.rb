c405303cdb653f6723ceca4499efe9974600577d
Ashar <sarah.eggleston@alliants.com>
2017-05-08 13:31:44 +0100

    Make mailchimp connection async

---
+++ app/jobs/mailchimp_job.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+class MailchimpJob < ActiveJob::Base
+  queue_as :default
+
+  def perform(list_id, email, merge_fields)
+    response = client.lists(list_id).members(md5_hashed_email(email)).upsert(
+      body: {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: merge_fields
+      }
+    )
+  end
+
+  private
+
+  def md5_hashed_email(email)
+    Digest::MD5.hexdigest(email.downcase)
+  end
+
+  def client
+    Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_key, symbolize_keys: true)
+  end
+end
--- app/services/marketing_mail_service.rb
+++ app/services/marketing_mail_service.rb
@@ -3,25 +3,10 @@ module MarketingMailService
   module_function
 
   def call(plot_residency, status)
-    response = test_list.members(md5_hashed_email(plot_residency.email)).upsert(
-      body: {
-        email_address: plot_residency.email,
-        status: "subscribed",
-        merge_fields: {HOOZSTATUS: status, FNAME: plot_residency.first_name, WRONG: plot_residency.last_name}
-      }
+    MailchimpJob.perform_later("91418e8697", plot_residency.email, {
+      HOOZSTATUS: status,
+      FNAME: plot_residency.first_name,
+      LNAME: plot_residency.last_name}
     )
   end
-
-  def md5_hashed_email(email)
-    Digest::MD5.hexdigest(email.downcase)
-  end
-
-  def test_list
-    client.lists("91418e8697")
-  end
-
-  def client
-    Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_key, symbolize_keys: true)
-  end
 end
-
