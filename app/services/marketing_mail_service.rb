43fbd1e3c256c28a305cb096ffa0ebf638ecece5
Ashar <sarah.eggleston@alliants.com>
2017-05-09 08:55:43 +0100

    Rubocop kept reverting my braces

--- app/services/marketing_mail_service.rb
+++ app/services/marketing_mail_service.rb
@@ -3,10 +3,12 @@ module MarketingMailService
   module_function
 
   def call(plot_residency, status)
+    merge_fields = { HOOZSTATUS: status,
+                     FNAME: plot_residency.first_name,
+                     LNAME: plot_residency.last_name }
+
     MailchimpJob.perform_later("91418e8697",
                                plot_residency.email,
-                               HOOZSTATUS: status,
-                               FNAME: plot_residency.first_name,
-                               LNAME: plot_residency.last_name)
+                               merge_fields)
   end
 end
