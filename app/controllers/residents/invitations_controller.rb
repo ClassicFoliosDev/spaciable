226c6da9b73f5c89abbb917399303ab15621fa3d
Ashar <sarah.eggleston@alliants.com>
2017-05-09 09:21:00 +0100

    Update homeowner status to activated when the user logs in for the first time

--- app/controllers/residents/invitations_controller.rb
+++ app/controllers/residents/invitations_controller.rb
@@ -2,5 +2,14 @@
 module Residents
   class InvitationsController < Devise::InvitationsController
     layout "accept"
+
+    def update
+      super
+
+      merge_fields = { HOOZSTATUS: "activated" }
+      MailchimpJob.perform_later("91418e8697",
+                                 current_resident.email,
+                                 merge_fields)
+    end
   end
 end
