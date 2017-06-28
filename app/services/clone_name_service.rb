f995df9ff2850cfda1d2e55000e5b776d646c9f3
Ashar <sarah.eggleston@alliants.com>
2017-06-28 13:25:26 +0100

    Refactor, rubocop doesn't like the return

--- app/services/clone_name_service.rb
+++ app/services/clone_name_service.rb
@@ -2,10 +2,20 @@
 module CloneNameService
   module_function
 
-  # rubocop:disable MethodLength
-  # Splitting this method would be complex because the variables calculated in the top
-  # section are used in the lower section
   def call(old_name)
+    base_name, new_number = init(old_name)
+
+    existing_unit_type = true
+    while existing_unit_type
+      new_number += 1
+      name = base_name + new_number.to_s
+      existing_unit_type = UnitType.find_by_name(name)
+    end
+
+    name
+  end
+
+  def init(old_name)
     last_index = old_name.length - 1
     last_char = old_name[last_index]
 
@@ -21,14 +31,6 @@ module CloneNameService
       new_number = old_number.to_i
     end
 
-    existing_unit_type = true
-    while existing_unit_type
-      new_number += 1
-      name = base_name + new_number.to_s
-      existing_unit_type = UnitType.find_by_name(name)
-    end
-
-    name
+    return base_name, new_number
   end
-  # rubocop:enable MethodLength
 end
