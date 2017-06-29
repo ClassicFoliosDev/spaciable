e3fbccc8fe571a36bdca628639f4006842994939
Ashar <sarah.eggleston@alliants.com>
2017-06-29 15:20:05 +0100

    Rename things in cloneNameService

--- app/services/clone_name_service.rb
+++ app/services/clone_name_service.rb
@@ -3,34 +3,38 @@ module CloneNameService
   module_function
 
   def call(old_name)
-    base_name, new_number = init(old_name)
+    base_name, base_number = calculate_base(old_name)
 
     existing_unit_type = true
     while existing_unit_type
-      new_number += 1
-      name = base_name + new_number.to_s
+      base_number += 1
+      name = base_name + base_number.to_s
       existing_unit_type = UnitType.find_by_name(name)
     end
 
     name
   end
 
-  def init(old_name)
+  private
+
+  module_function
+
+  def calculate_base(old_name)
     last_index = old_name.length - 1
     last_char = old_name[last_index]
 
     if last_char =~ /[[:alpha:]]/
       base_name = old_name + " "
-      new_number = 0
+      base_number = 0
     else
       numbers_in_string = old_name.scan(/\d+/)
       old_number = numbers_in_string[numbers_in_string.length - 1]
       old_number_index = old_name.index(old_number.to_s)
 
       base_name = old_name[0, old_number_index]
-      new_number = old_number.to_i
+      base_number = old_number.to_i
     end
 
-    [base_name, new_number]
+    [base_name, base_number]
   end
 end
