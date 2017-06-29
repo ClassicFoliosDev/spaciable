ff0006185b97edcbde28d6d543ff8a324806a7d5
Ashar <sarah.eggleston@alliants.com>
2017-06-29 16:59:45 +0100

    Hooz 425 clone unit type (#322)
    Improve number increment process for clone name service
    * Should now be possible to clone the same unit type multiple times
    * Related test improvements

--- spec/services/clone_name_service_spec.rb
+++ spec/services/clone_name_service_spec.rb
@@ -17,4 +17,73 @@ RSpec.describe CloneNameService do
       expect(result).to eq("A name that ends with 112")
     end
   end
+
+  context "name ending in 999" do
+    it "increments the end" do
+      result = described_class.call("A name that ends with 999")
+
+      expect(result).to eq("A name that ends with 1000")
+    end
+  end
+
+  context "name ending in 10" do
+    it "increments the end" do
+      result = described_class.call("A name that ends with 10")
+
+      expect(result).to eq("A name that ends with 11")
+    end
+  end
+
+  context "name ending in 210" do
+    it "increments the end" do
+      result = described_class.call("A name that ends with 210")
+
+      expect(result).to eq("A name that ends with 211")
+    end
+  end
+
+  context "name ending in 99" do
+    it "increments the end" do
+      result = described_class.call("A name that ends with 99")
+
+      expect(result).to eq("A name that ends with 100")
+    end
+  end
+
+  context "name ending in 9" do
+    it "increments the end" do
+      result = described_class.call("A name that ends with 9")
+
+      expect(result).to eq("A name that ends with 10")
+    end
+  end
+
+  context "name with several numbers ending in 0" do
+    it "increments the end" do
+      result = described_class.call("A name with 44 48960 that ends with 190")
+
+      expect(result).to eq("A name with 44 48960 that ends with 191")
+    end
+  end
+
+  context "name with several numbers ending in a letter" do
+    it "increments the end" do
+      result = described_class.call("A name with 44 and 76542 that ends with a letter")
+
+      expect(result).to eq("A name with 44 and 76542 that ends with a letter 1")
+    end
+  end
+
+  context "clone in the database already" do
+    it "increments an extra time" do
+      unit_type = create(:unit_type)
+      new_name = unit_type.name + " 1"
+      create(:unit_type, name: new_name)
+
+      result = described_class.call(unit_type.name)
+
+      next_new_name = unit_type.name + " 2"
+      expect(result).to eq(next_new_name)
+    end
+  end
 end
