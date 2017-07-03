ad7457bab8121d69ef9bf7d500c262ab6b408395
Ashar <sarah.eggleston@alliants.com>
2017-07-03 15:04:13 +0100

    Room not needed for finishes

--- db/migrate/20161103133630_create_finishes.rb
+++ db/migrate/20161103133630_create_finishes.rb
@@ -1,7 +1,6 @@
 class CreateFinishes < ActiveRecord::Migration[5.0]
   def change
     create_table :finishes do |t|
-      t.belongs_to :room, foreign_key: true
       t.string :name
       t.string :category
 
