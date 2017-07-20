3b0f4cd348aa1ce184dbad458fa8c4df7b997f5d
Ashar <sarah.eggleston@alliants.com>
2017-07-20 15:40:53 +0100

    Hooz 537 appliances finishes fixes (#346)
    Set of small fixes:
    
    * Validate manufacturers for unique name regardless of case
    * Fix sort order for appliance manufacturers listed in appliance category
    * Remove category from homeowner library contents
    * Make dropdowns blank if dependent select has not been chosen
    * Don't empty the collections if user has already selected
    * Always use full_name for appliance name

--- app/models/appliance_manufacturer.rb
+++ app/models/appliance_manufacturer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 class ApplianceManufacturer < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 
   delegate :to_s, to: :name
 end
--- app/models/finish_manufacturer.rb
+++ app/models/finish_manufacturer.rb
@@ -4,7 +4,7 @@ class FinishManufacturer < ApplicationRecord
   has_many :finish_types, through: :finish_types_manufacturer
   has_many :finish_categories, through: :finish_types
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :finish_types, length: { minimum: 1 }
 
   delegate :to_s, to: :name
