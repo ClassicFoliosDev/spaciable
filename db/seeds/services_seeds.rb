a5531d859d440279bfe6c0e64e15ed8bc681afcb
Ashar <sarah.eggleston@alliants.com>
2017-11-27 10:45:17 +0000

    HOOZ 696 Change services text (#430)
    Change the translated title/description for services
    Change the contents of the DB services
    
    Also
    * Change mobile pop-up lifespan to infinite

--- db/seeds/services_seeds.rb
+++ db/seeds/services_seeds.rb
@@ -2,12 +2,21 @@ STDOUT.puts <<-INFO
    Creating homeowner services
 INFO
 
-Service.find_or_create_by(name: "Financial services", description: "Mortgage brokers, solicitors")
-Service.find_or_create_by(name: "Utilities", description: "TV (Digital ariel, satellite, cable), Broadband, electricity, gas, water, telephone, residential parking permits")
+financial_services = Service.find_by(name: "Financial services")
+financial_services.update_attributes(name: "Financial Services") if financial_services
+
+other_services = Service.find_by(name: "Other services")
+other_services.update_attributes(name: "Other Services", description: "Security systems, internet of things (smart home) providers, furniture packs, appliances, change of address service") if other_services
+
+utilities = Service.find_by(name: "Utilities")
+utilities.update_attributes(description: "TV (digital aeriel, satellite, cable), broadband, electricity, gas, water, telephone, residential parking permits") if utilities
+
+Service.find_or_create_by(name: "Financial Services", description: "Mortgage brokers, solicitors")
+Service.find_or_create_by(name: "Utilities", description: "TV (digital aeriel, satellite, cable), broadband, electricity, gas, water, telephone, residential parking permits")
 Service.find_or_create_by(name: "Insurance", description: "Building, content, car, security")
 Service.find_or_create_by(name: "Removals", description: "Removal firms, packing services")
 Service.find_or_create_by(name: "Maintenance providers", description: "Landscaping, interior designers, electricians, plumbers, builders, conservatory suppliers, gardeners, window cleaners")
-Service.find_or_create_by(name: "Other services", description: "Security systems, IOT (Smart home) provider, furniture packs, appliances, change of address service")
+Service.find_or_create_by(name: "Other Services", description: "Security systems, internet of things (smart home) providers, furniture packs, appliances, change of address service")
 
 STDOUT.puts <<-INFO
    Finished creating homeowner services
