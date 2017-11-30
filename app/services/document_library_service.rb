3dab3a14a6b84866835451b49145a8490bacd591
Ashar <sarah.eggleston@alliants.com>
2017-11-30 09:21:43 +0000

    Carrierwave aws fix (#440)
    Carrierwave AWS broke the build_preview code seriously.
    
    This PR isn't a full fix, previews still don't work, but it should remove the worst symptoms, instead you just won't see previews.
    
    This is so we can get the fix into staging for CF to test - if they give us good feedback, I'll look into the proper preview fix: but it will need time invested.
    
    Also fixed:
    
    *   RACK_ENV change wasn't good, and I think isn't needed, will use this PR to test that as well.

--- app/services/document_library_service.rb
+++ app/services/document_library_service.rb
@@ -8,18 +8,18 @@ module DocumentLibraryService
   def call(documents, appliances)
     alldocuments = documents.map do |d|
       { name: d.title, link: d.file.url, category: d.category, id: d.id,
-        thumb: build_preview(d.file), timestamp: d.updated_at }
+        thumb: build_preview_url(d.file), timestamp: d.updated_at }
     end
     manuals = appliances.map do |a|
       if a.manual?
         { name: a.to_s, link: a.manual.url, parent: a, category: I18n.t(".manual"),
-          thumb: build_preview(a.manual), timestamp: a.updated_at }
+          thumb: build_preview_url(a.manual), timestamp: a.updated_at }
       end
     end
     guides = appliances.map do |a|
       if a.guide?
         { name: a.to_s, link: a.guide.url, parent: a, category: I18n.t(".guide"),
-          thumb: build_preview(a.guide), timestamp: a.updated_at }
+          thumb: build_preview_url(a.guide), timestamp: a.updated_at }
       end
     end
 
@@ -41,7 +41,8 @@ module DocumentLibraryService
   # Without overriding the content_type, the headers would return 'application/pdf'
   # for the preview image, and all browsers except for Safari will show the image
   # (regardless of the content type header). This fixes the preview thumbs for Safari.
-  def build_preview(file)
-    file.preview&.url(query: { "response-content-type" => "image/jpeg" })
+  def build_preview_url(file)
+    # TODO: after bulk upload testing
+    file.preview&.url
   end
 end
