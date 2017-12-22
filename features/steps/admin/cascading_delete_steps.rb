f1ab2254f72220a922661ad2dbd5e740f79c7fd3
Ashar <sarah.eggleston@alliants.com>
2017-12-22 15:37:53 +0000

    Make sure button footer doesn't lie over copyright footer (#466)
    Make sure button footer doesn't lie over copyright footer
    
    Also
    * Attempt to make cascading delete test more reliable

--- features/steps/admin/cascading_delete_steps.rb
+++ features/steps/admin/cascading_delete_steps.rb
@@ -3,7 +3,7 @@
 Then(/^I should see most of the children have been removed from the database$/) do
   # Most children should be removed
 
-  sleep 0.4
+  sleep 0.7
   expect(Developer.count).to eq(0)
   expect(Development.count).to eq(0)
 
