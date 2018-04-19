e9113a553cdd0ed3e118cb5c3942e91b2cec3b7a
Ashar <sarah.eggleston@alliants.com>
2018-04-19 10:14:10 +0100

    JS standard fixes (#534)
    Fixes recommended by JS standard linter - in notifications.js many of these were legacy fixes
    
    These changes should have no impact on end users

--- app/assets/javascripts/admin/analytics.js
+++ app/assets/javascripts/admin/analytics.js
@@ -1,27 +1,23 @@
-/* global $ */
-
 (function (document, $) {
   'use strict'
 
-  $(document).on('mouseup', '.report-targets .ui-menu-item', function(event) {
+  $(document).on('mouseup', '.report-targets .ui-menu-item', function (event) {
     var eventVal = event.target.innerHTML
-    var $parent = $(event.target).closest(".select-container")
+    var $parent = $(event.target).closest('.select-container')
     var $submitButton
 
-    if ($parent.hasClass("developer-id")) {
-      $submitButton = $(".developer-csv")
-    } else if ($parent.hasClass("development-id")) {
-      $submitButton = $(".development-csv")
+    if ($parent.hasClass('developer-id')) {
+      $submitButton = $('.developer-csv')
+    } else if ($parent.hasClass('development-id')) {
+      $submitButton = $('.development-csv')
     } else {
       return
     }
 
-    if (eventVal == "Choose...") {
+    if (eventVal === 'Choose...') {
       $submitButton.prop('disabled', true)
-    }
-    else {
+    } else {
       $submitButton.prop('disabled', false)
     }
   })
-
 })(document, window.jQuery)
--- app/assets/javascripts/admin/notifications.js
+++ app/assets/javascripts/admin/notifications.js
@@ -1,132 +1,133 @@
-document.addEventListener("turbolinks:load", function () {
+/* global $, clearFields, setFields */
+
+document.addEventListener('turbolinks:load', function () {
   (function () {
-    var $developerSelect = $('.notification_developer_id select');
+    var $developerSelect = $('.notification_developer_id select')
 
     if ($developerSelect.length === 0) {
       $developerSelect = $('.report_developer_id select')
     }
 
     if ($developerSelect.length > 0) {
-      $developerSelect.selectmenu(developerSelectmenuCallbacks());
+      $developerSelect.selectmenu(developerSelectmenuCallbacks())
     }
   })(
     initialiseDeveloper()
-  );
+  )
 
-  function developerSelectmenuCallbacks() {
+  function developerSelectmenuCallbacks () {
     return {
       create: function (event, ui) {
-        var $selectInput = $(event.target);
-        var developerId = $selectInput.val();
+        var $selectInput = $(event.target)
+        var developerId = $selectInput.val()
 
         if (developerId) {
-          var $divisionSelect = $('.notification_division_id select');
-          fetchDeveloperResources({ developerId: developerId });
-          $divisionSelect.selectmenu(divisionSelectmenuCallbacks(developerId));
+          var $divisionSelect = $('.notification_division_id select')
+          fetchDeveloperResources({ developerId: developerId })
+          $divisionSelect.selectmenu(divisionSelectmenuCallbacks(developerId))
         }
       },
       select: function (event, ui) {
-        var developerId = ui.item.value;
+        var developerId = ui.item.value
 
-        if (developerId !== "") {
-          fetchDivisionResources({ developerId: developerId });
-          fetchDevelopmentResources({ developerId: developerId });
-          fetchPhaseResources({ developerId: developerId });
+        if (developerId !== '') {
+          fetchDivisionResources({ developerId: developerId })
+          fetchDevelopmentResources({ developerId: developerId })
+          fetchPhaseResources({ developerId: developerId })
         } else {
-          fetchDivisionResources();
-          fetchDevelopmentResources();
-          fetchPhaseResources();
+          fetchDivisionResources()
+          fetchDevelopmentResources()
+          fetchPhaseResources()
         }
       }
     }
   };
 
-  function divisionSelectmenuCallbacks(developerId) {
+  function divisionSelectmenuCallbacks (developerId) {
     return {
       create: function (event, ui) {
-        var $selectInput = $(event.target);
-        var divisionId = $selectInput.val();
-        var $developmentSelect = $('.notification_development_id select');
+        var $selectInput = $(event.target)
+        var divisionId = $selectInput.val()
+        var $developmentSelect = $('.notification_development_id select')
         if ($developmentSelect.length === 0) {
           $developmentSelect = $('.report_development_id select')
         }
+        var $phaseSelect = $('.notification_phase_id select')
 
-        if (divisionId !== "") {
-          fetchDivisionResources({ developerId: developerId, divisionId: divisionId});
-          $developmentSelect.selectmenu(developmentSelectmenuCallbacks(developerId, divisionId));
-          var $phaseSelect = $('.notification_phase_id select')
+        if (divisionId !== '') {
+          fetchDivisionResources({developerId: developerId, divisionId: divisionId})
+          $developmentSelect.selectmenu(developmentSelectmenuCallbacks(developerId, divisionId))
           if ($phaseSelect.length > 0) {
-            $phaseSelect.selectmenu(phaseSelectmenuCallbacks(developerId, divisionId));
+            $phaseSelect.selectmenu(phaseSelectmenuCallbacks(developerId, divisionId))
           }
-        } else if (divisionId === ""){
-          fetchDivisionResources({ developerId: developerId });
-          $developmentSelect.selectmenu(developmentSelectmenuCallbacks(developerId));
-          var $phaseSelect = $('.notification_phase_id select')
+        } else if (divisionId === '') {
+          fetchDivisionResources({ developerId: developerId })
+          $developmentSelect.selectmenu(developmentSelectmenuCallbacks(developerId))
           if ($phaseSelect.length > 0) {
-            $phaseSelect.selectmenu(phaseSelectmenuCallbacks(developerId));
+            $phaseSelect.selectmenu(phaseSelectmenuCallbacks(developerId))
           }
         };
       },
       select: function (event, ui) {
-        var divisionId = ui.item.value;
+        var divisionId = ui.item.value
 
         if (divisionId) {
-          fetchDevelopmentResources({ divisionId: divisionId });
-          fetchPhaseResources({ divisionId: divisionId });
+          fetchDevelopmentResources({ divisionId: divisionId })
+          fetchPhaseResources({ divisionId: divisionId })
         } else {
-          var $developerSelect = $('.notification_developer_id select');
+          var $developerSelect = $('.notification_developer_id select')
 
           if ($developerSelect.length === 0) {
             $developerSelect = $('.report_developer_id select')
           }
           var developerId = $developerSelect.val()
           if (developerId) {
-            fetchDevelopmentResources({ developerId: developerId });
-            fetchPhaseResources({ developerId: developerId });
+            fetchDevelopmentResources({ developerId: developerId })
+            fetchPhaseResources({ developerId: developerId })
           }
         };
       }
-    };
+    }
   };
 
-  function developmentSelectmenuCallbacks(developerId, divisionId) {
+  function developmentSelectmenuCallbacks (developerId, divisionId) {
     return {
       create: function (event, ui) {
-        var $selectInput = $(event.target);
-        var developmentId = $selectInput.val();
+        var $selectInput = $(event.target)
+        var developmentId = $selectInput.val()
 
-        if (developmentId !== "") {
+        if (developmentId !== '') {
           fetchDevelopmentResources({
             developerId: developerId,
             divisionId: divisionId,
             developmentId: developmentId
-          });
+          })
         } else if (divisionId) {
           fetchDevelopmentResources({
             developerId: developerId,
             divisionId: divisionId
-          });
+          })
         } else {
           fetchDevelopmentResources({
             developerId: developerId
-          });
+          })
         };
       },
       select: function (event, ui) {
-        var developmentId = ui.item.value;
+        var developmentId = ui.item.value
 
-        if (developmentId !== "") {
-          fetchPhaseResources({ developmentId: developmentId });
+        if (developmentId !== '') {
+          fetchPhaseResources({ developmentId: developmentId })
         };
       }
-    };
+    }
   };
 
-  function phaseSelectmenuCallbacks(developerId, divisionId, developmentId) {
+  function phaseSelectmenuCallbacks (developerId, divisionId, developmentId) {
     return {
       create: function (event, ui) {
-        var $selectInput = $(event.target);
-        var phaseId = $selectInput.val();
+        var $selectInput = $(event.target)
+        var phaseId = $selectInput.val()
 
         if (phaseId) {
           fetchPhaseResources({
@@ -134,86 +135,85 @@ document.addEventListener("turbolinks:load", function () {
             divisionId: divisionId,
             developmentId: developmentId,
             phaseId: phaseId
-          });
+          })
         } else if (developmentId) {
           fetchPhaseResources({
             developmentId: developmentId
-          });
+          })
         } else if (divisionId) {
           fetchPhaseResources({
             divisionId: divisionId
-          });
+          })
         } else {
           fetchPhaseResources({
             developerId: developerId
-          });
+          })
         };
       }
-    };
+    }
   };
 
-  function fetchDivisionResources(data) {
-    var $divisionId = $('.notification_division_id');
+  function fetchDivisionResources (data) {
+    var $divisionId = $('.notification_division_id')
     if ($divisionId.length === 0) {
-      $divisionId = $('.report_division_id');
+      $divisionId = $('.report_division_id')
     }
 
+    var divisionSelect = clearFields($divisionId)
+    var url = '/admin/divisions'
 
-    var division_select = clearFields($divisionId);
-    var url = '/admin/divisions';
-
-    setFields(division_select, url, data);
+    setFields(divisionSelect, url, data)
   };
 
-  function fetchDevelopmentResources(data) {
-    var $developmentId = $('.notification_development_id');
-      if ($developmentId.length === 0) {
-        $developmentId = $('.report_development_id');
-      }
+  function fetchDevelopmentResources (data) {
+    var $developmentId = $('.notification_development_id')
+    if ($developmentId.length === 0) {
+      $developmentId = $('.report_development_id')
+    }
 
-    var development_select = clearFields($developmentId);
-    var url = '/admin/developments';
+    var developmentSelect = clearFields($developmentId)
+    var url = '/admin/developments'
 
-    setFields(development_select, url, data);
+    setFields(developmentSelect, url, data)
   };
 
-  function fetchPhaseResources(data) {
-    var $phaseId = $('.notification_phase_id');
+  function fetchPhaseResources (data) {
+    var $phaseId = $('.notification_phase_id')
     if ($phaseId.length > 0) {
-      var phaseSelect = clearFields($phaseId);
-      var url = '/admin/phases';
-      setFields(phaseSelect, url, data);
+      var phaseSelect = clearFields($phaseId)
+      var url = '/admin/phases'
+      setFields(phaseSelect, url, data)
     }
   };
 
-  function initialiseDeveloper() {
-    var $developerSelect = $('.notification_developer_id select');
+  function initialiseDeveloper () {
+    var $developerSelect = $('.notification_developer_id select')
     if ($developerSelect.length === 0) {
       $developerSelect = $('.report_developer_id select')
     }
 
     if ($developerSelect.length > 0) {
       if ($developerSelect[0].options.length < 2) {
-        var $divisionSelect = $('.notification_division_id select');
+        var $divisionSelect = $('.notification_division_id select')
         if ($divisionSelect.length === 0) {
           $divisionSelect = $('.report_division_id select')
         }
 
-        fetchDeveloperResources();
-        $divisionSelect.selectmenu(divisionSelectmenuCallbacks());
+        fetchDeveloperResources()
+        $divisionSelect.selectmenu(divisionSelectmenuCallbacks())
       }
     }
   }
 
-  function fetchDeveloperResources(data) {
-    var $developerId = $('.notification_developer_id');
+  function fetchDeveloperResources (data) {
+    var $developerId = $('.notification_developer_id')
     if ($developerId.length === 0) {
-      $developerId = $('.report_developer_id');
+      $developerId = $('.report_developer_id')
     }
 
-    var developer_select = clearFields($developerId);
-    var url = '/admin/developers';
+    var developerSelect = clearFields($developerId)
+    var url = '/admin/developers'
 
-    setFields(developer_select, url, data);
+    setFields(developerSelect, url, data)
   };
-});
+})
