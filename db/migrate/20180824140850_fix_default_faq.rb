e9a4cd2a682c295bffa357f1640ee63c49cfc134
Ashar <segglestn@gmail.com>
2018-08-29 10:15:57 +0100

    Make sure migration does not error if FAQ is missing (#594)

--- db/migrate/20180824140850_fix_default_faq.rb
+++ db/migrate/20180824140850_fix_default_faq.rb
@@ -2,6 +2,7 @@ class FixDefaultFaq < ActiveRecord::Migration[5.0]
   def up
     tv_faq = DefaultFaq.find_by(question: %(Will my existing TV Licence cover my new home?))
 
+    return unless tv_faq
     tv_faq.update_attributes(answer: %(<p>Please be aware that your television licence doesn&rsquo;t automatically move with you, and it&rsquo;s important that you notify TV Licensing so they can transfer your licence to your new address:</p>
 
                                        <p>Telephone: 0300 790 6165<br>
