31aec05bb285f6433e3e540674de2a395687d630
Ashar <segglestn@gmail.com>
2018-08-30 17:44:11 +0100

    Hooz 262 delete private files (#600)
    Rename the file before sending
    
    Use translations for text
    
    Still still testing in staging. If this works, it might be the last set of updates to the weTransfer code, and next PR will move on to delete.

--- app/services/resident_reset_service.rb
+++ app/services/resident_reset_service.rb
@@ -44,30 +44,59 @@ module ResidentResetService
   end
 
   def transfer_private_files(resident)
-    sent_file_url = transfer_files(resident)
-
     log_path = Rails.root.join("log", "closing_file_transfer.log")
     logger = Logger.new(log_path)
     logger.info("==== Resident #{resident.email} ====")
-    logger.info("Transferred #{resident.private_documents.count} files to")
-    logger.info(sent_file_url)
+
+    if Rails.application.secrets.we_transfer_key.blank?
+      logger.error("Unable to transfer files, no API key for transfer")
+      return
+    end
+
+    transfer_url = transfer_files(resident, logger)
+    logger.info(transfer_url)
   end
 
-  def transfer_files(resident)
-    return unless Rails.application.secrets.we_transfer_key
-
+  def transfer_files(resident, logger)
     @file_client = WeTransferClient.new(api_key: Rails.application.secrets.we_transfer_key)
 
-    name = "Close account"
-    description = "Files from your closed account"
+    name = resident.to_s
+    description = I18n.t("close_account_files_description")
+    hash = tmp_folder(resident.email)
+
     transfer = @file_client.create_transfer(name: name, description: description) do |upload|
       resident.private_documents.each do |document|
-        temp_file = MiniMagick::Image.open(document.file.url)
-        upload.add_file_at(path: temp_file.path)
+        cloned_file_path = file_path(document, hash, logger)
+        upload.add_file_at(path: cloned_file_path)
       end
-      upload.add_web_url(url: "https://hoozzi.wetransfer.com", title: "Hoozzi file transfers")
+      upload.add_web_url(url: "https://wetransfer.com", title: I18n.t("file_transfer"))
     end
 
     transfer.shortened_url
+  end
+
+  def tmp_folder(email)
+    # The file will be stored as "mini_magic_<timestamp>" by default, so we need to save it with
+    # a more user friendly name, reusing the original file name. File names are not guaranteed
+    # unique, so for safety create a temp folder to store it in
+
+    hash = Digest::MD5.hexdigest(email)
+    tmp_folder = Rails.root.join("tmp", hash)
+    FileUtils.mkdir_p(tmp_folder) unless File.directory?(tmp_folder)
+
+    hash
+  end
+
+  def file_path(document, hash, logger)
+    temp_file = MiniMagick::Image.open(document.file.url)
+    cloned_file = temp_file.clone
+    document_long_name = document.file.url.split("file/#{document.id}/").last
+    document_name = document_long_name.split("?X-Amz-Algorithm").first
+
+    save_path = Rails.root.join("tmp", hash, document_name)
+    cloned_file.write(save_path)
+
+    logger.info("Transferring #{cloned_file.path}")
+    cloned_file.path
   end
 end
