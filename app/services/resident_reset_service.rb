b947dfb3a7ae856aa27523f86154853287193dbd
Ashar <segglestn@gmail.com>
2018-08-30 16:01:23 +0100

    WeTransfer might be sending (#599)
    Still prototyping, weTransfer file upload might be working, want to capture this quickly - but it isn't yet the final version. As before, I can not test weTransfer on local environment, only on Staging.
    
    The URL is not correct, and the file name and other parameters may still change: but the file should be transferred successfully with this build.
    
    Still no delete of the originals yet.

--- app/services/resident_reset_service.rb
+++ app/services/resident_reset_service.rb
@@ -19,14 +19,12 @@ module ResidentResetService
   end
 
   def reset_resident(resident, plots)
-    transfer_files(resident) if resident.private_documents.any?
+    transfer_private_files(resident) if resident.private_documents.any?
+    update_resident_params(resident)
+    update_mailchimp(resident, plots)
+  end
 
-    resident.developer_email_updates = false
-    resident.isyt_email_updates = false
-    resident.telephone_updates = false
-    resident.post_updates = false
-    resident.save(validate: false)
-
+  def update_mailchimp(resident, plots)
     response = ""
     plots.each do |plot|
       response = Mailchimp::MarketingMailService.call(resident,
@@ -37,17 +35,39 @@ module ResidentResetService
     response
   end
 
+  def update_resident_params(resident)
+    resident.developer_email_updates = false
+    resident.isyt_email_updates = false
+    resident.telephone_updates = false
+    resident.post_updates = false
+    resident.save(validate: false)
+  end
+
+  def transfer_private_files(resident)
+    sent_file_url = transfer_files(resident)
+
+    log_path = Rails.root.join("log", "closing_file_transfer.log")
+    logger = Logger.new(log_path)
+    logger.info("==== Resident #{resident.email} ====")
+    logger.info("Transferred #{resident.private_documents.count} files to")
+    logger.info(sent_file_url)
+  end
+
   def transfer_files(resident)
     return unless Rails.application.secrets.we_transfer_key
 
     @file_client = WeTransferClient.new(api_key: Rails.application.secrets.we_transfer_key)
 
+    name = "Close account"
     description = "Files from your closed account"
-    @file_client.create_transfer(name: "Close account", description: description) do |upload|
+    transfer = @file_client.create_transfer(name: name, description: description) do |upload|
       resident.private_documents.each do |document|
-        upload.add_file_at(path: document.file.url)
+        temp_file = MiniMagick::Image.open(document.file.url)
+        upload.add_file_at(path: temp_file.path)
       end
       upload.add_web_url(url: "https://hoozzi.wetransfer.com", title: "Hoozzi file transfers")
     end
+
+    transfer.shortened_url
   end
 end
