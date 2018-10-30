62f8c20eef86f8ec0ef1ccf97a52e62cb07333ea
egglestn <segglestn@gmail.com>
2018-10-30 13:02:14 +0000

    JS Standard fixes to new file

--- app/assets/javascripts/admin/production_updates.js
+++ app/assets/javascripts/admin/production_updates.js
@@ -1,34 +1,34 @@
-/* global $ */
+(function (document, $) {
+  'use strict'
 
-$(document).on('change', ".update", event => {
-  const $input = $(event.target);
-  const $form = $input.closest('form');
-  const method = $('input[name="_method"]').val();
-  const params = buildParams($input, $form);
+  $(document).on('change', '.update', function (event) {
+    const $input = $(event.target)
+    const $form = $input.closest('form')
+    const method = $('input[name="_method"]').val()
+    const params = 'TODO'
 
-  $.ajax({
-    url: $form.attr('action'),
-    method: method,
-    dataType: 'json',
-    data: params,
-    success: function(response) {
-      var $flash = $('.flash');
-      $flash.empty();
+    $.ajax({
+      url: $form.attr('action'),
+      method: method,
+      dataType: 'json',
+      data: params,
+      success: function (response) {
+        var $flash = $('.flash')
+        $flash.empty()
 
-      updateResults(response);
-
-      var $notice = document.createElement('p');
-      $notice.className = 'notice';
-      $notice.innerHTML = response.notice;
-      $flash.append($notice);
-    },
-    error: function(response) {
-      var $flash = $('.flash');
-      $flash.empty();
-      var $notice = document.createElement('p');
-      $notice.className = 'alert';
-      $notice.innerHTML = response.responseJSON['error'];
-      $flash.append($notice);
-    }
-  });
-});
+        var $notice = document.createElement('p')
+        $notice.className = 'notice'
+        $notice.innerHTML = response.notice
+        $flash.append($notice)
+      },
+      error: function (response) {
+        var $flash = $('.flash')
+        $flash.empty()
+        var $notice = document.createElement('p')
+        $notice.className = 'alert'
+        $notice.innerHTML = response.responseJSON['error']
+        $flash.append($notice)
+      }
+    })
+  })
+})
