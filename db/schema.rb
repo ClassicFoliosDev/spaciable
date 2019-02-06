0f81a1494f6b6a45453caeeb15e30ddbad57331b
ClassicFoliosDev <42801626+ClassicFoliosDev@users.noreply.github.com>
2019-02-06 15:48:05 +0000

    HOOZ-308 (#702)
    * Development defaults to Phases tab
    
    * NHBC dropdown for Development
    
    * Business info in Developer/Billing reports
    
    * Fixed broken tests for Development default to Phases tab
    
    * Removed redundant redirects to Phases tab in feature tests
    
    * Start Library tab fixes
    
    * Default to leftmost tab in Contacts and Library. Management tab always visible in Contacts
    
    * HOOZ-248 completed, tabs default to leftmost in Contacts and Library
    
    * Remove TidyPreviousMigrations migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190205121954) do
+ActiveRecord::Schema.define(version: 20190205100802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
