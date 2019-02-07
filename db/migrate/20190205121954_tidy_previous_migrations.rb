a9db77b104d413f86cf1ecbe72922a85fa2b0507
ClassicFoliosDev <42801626+ClassicFoliosDev@users.noreply.github.com>
2019-02-07 08:00:33 +0000

    Fixing the Migration Issue (#703)
    * please
    
    * pretty please
    
    * rebase

--- db/migrate/20190205121954_tidy_previous_migrations.rb
+++
@@ -1,8 +0,0 @@
-class TidyPreviousMigrations < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :countries
-    remove_column :how_tos, :country_id
-    remove_column :default_faqs, :country_id
-    remove_column :developers, :country_id
-  end
-end
