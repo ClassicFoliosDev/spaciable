5c39aa813906d5d1ec07910abfa4ebfbd6c2e0d4
ClassicFoliosDev <42801626+ClassicFoliosDev@users.noreply.github.com>
2019-08-28 11:27:35 +0100

    uncomment plots migration (#782)

--- db/migrate/20190828093206_add_lettings.rb
+++ db/migrate/20190828093206_add_lettings.rb
@@ -1,7 +1,7 @@
 class AddLettings < ActiveRecord::Migration[5.0]
   def change
-    #add_column :plots, :letable, :boolean, default: false
-    #add_column :plots, :let, :boolean, default: false
+    add_column :plots, :letable, :boolean, default: false
+    add_column :plots, :let, :boolean, default: false
     add_column :plots, :letable_type, :integer
     add_column :plots, :letter_type, :integer
   end
