8481d0213749a0e0bf79317732781684c6dbc64e
jason.mardell@gmail.com <biker.nut.9364>
2020-03-21 12:25:17 +0000

    Rooms now being destroyed rather than 'Archived'

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -41,16 +41,4 @@ RSpec.describe Room do
     end
   end
 
-  describe "#destroy" do
-    subject { FactoryGirl.create(:room, last_updated_by: create(:development_admin, permission_level: create(:development)).display_name) }
-
-    include_examples "archive when destroyed"
-    it_behaves_like "archiving is dependent on parent association", :developer
-    it_behaves_like "archiving is dependent on parent association", :division do
-      let(:unit_type) { create(:unit_type, development: create(:division_development)) }
-      subject { FactoryGirl.create(:room, unit_type: unit_type, last_updated_by: create(:development_admin, permission_level: create(:development)).display_name) }
-    end
-    it_behaves_like "archiving is dependent on parent association", :development
-    it_behaves_like "archiving is dependent on parent association", :unit_type
-  end
 end
