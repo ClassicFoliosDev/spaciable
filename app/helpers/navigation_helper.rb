af483482b5216e94372c5deea9ff92d9558787d6
Robyn <robyn@classicfolios.com>
2020-04-02 08:11:54 +0100

    menu redesign, desktop, add perks to navigation

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -10,4 +10,27 @@ module NavigationHelper
     developer = plot.developer
     return "?sf_name=#{name}&sf_email=#{email}&sf_telephone=#{phone}&sf_developer=#{developer}"
   end
+
+  def allocate_perk_type(current_resident, plot)
+    return unless current_resident
+    # if Premium is available on the plot for the current resident
+    if Vaboo.premium_perks_available?(current_resident, plot)
+      # has the plot reached legal completion?
+      if plot.completed?
+        # if another resident on the plot has activated Premium
+        if Vaboo.premium_perks_activated?(plot)
+          "basic"
+        # if no other resident on the plot has activated Premium
+        else
+          "premium"
+        end
+      # if Premium is available on the plot but the plot has not reached legal completion
+      else
+        "coming_soon"
+      end
+    # if Premium is not available on the plot regardless if the plot has reached legal completion
+    else
+      "basic"
+    end
+  end
 end
