572fd9ad216890cd35a030b79f789ce60fd5b596
jason.mardell@gmail.com <biker.nut.9364>
2020-04-20 09:47:38 +0100

    CAS test failure

--- features/steps/admin/log_steps.rb
+++ features/steps/admin/log_steps.rb
@@ -15,7 +15,7 @@ end
 And(/^I should see a (.*) (.*) log entry created by (.*)/) do |item, action, user|
   goto_ut_logs
   within ".record-list" do
-   find(:xpath, ".//tr[td//text()[contains(., '#{eval(item)}')] and contains(., '#{action}') and contains(., '#{eval(user)}')]")
+   find(:xpath, ".//tr[td//text()[contains(., '#{eval(item)}')] and contains(., '#{action.capitalize()}') and contains(., '#{eval(user)}')]")
   end
 end
 
