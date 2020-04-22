aba3b570fbe6312bd1336258dd38575db017ebef
Robyn <robyn@classicfolios.com>
2020-04-22 08:15:29 +0100

    timeline, shortcut button toggle, styling colours

--- app/assets/javascripts/homeowner/timeline.js
+++ app/assets/javascripts/homeowner/timeline.js
@@ -20,6 +20,28 @@
     }
   }
 
+  // show the shortcut links
+  $(document).on('click', '#timelineShortcuts', function (event) {
+    $(".shortcut-links").toggle()
+
+    // style the open/close button
+    $(".shortcut-button").toggleClass("close")
+
+    // toggle the fa icon when the button is opened/closed
+    var shortcuts = document.getElementById("timelineShortcuts")
+    var button = document.getElementById("shortcutButton")
+    if (shortcuts.classList.contains("close")) {
+      button.classList.add("fa-times")
+      button.classList.remove("fa-question")
+      // center the icon
+      button.style.paddingLeft = "1px"
+    } else {
+      button.classList.add("fa-question")
+      button.classList.remove("fa-times")
+      button.style.paddingLeft = "0"
+    }
+  })
+
   // add the fa icons to the stage tiles
   document.addEventListener('turbolinks:load', function () {
     var res = document.getElementById("iconReservation")
