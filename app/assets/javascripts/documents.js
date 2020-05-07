7ec5c68d733f84a544d7fae4d3f0fd502e72eab5
Robyn <robyn@classicfolios.com>
2020-05-07 13:39:10 +0100

    documents, hide dropdown when multiple files selected, reset dropdown value when hidden

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,20 +1,41 @@
-// hide the Guide selector if the Category selector does not have value my_home on selection Category option
+var category = '#documentCategorySelector'
+var guide = '#documentGuideSelector'
+
+// show or hide the guide selector on category change
 $(document).on('click', '#document_category-menu', function (event) {
-  if ($('#documentCategorySelector').find('option[value=my_home]')[0].selected) {
-    $('#documentGuideSelector').show()
-  } else {
-    $('#documentGuideSelector option')[0].selected = true
-    $('#documentGuideSelector').hide()
-  }
+  multipleFilesCategorySelection()
+})
+
+// show or hide the guide selector when selecting documents for upload
+$(document).on('change', '#document_files.file-upload', function (event) {
+  multipleFilesCategorySelection()
 })
 
 // hide the Guide selector if the Category selector does not have value my_home on page load
 document.addEventListener('turbolinks:load', function (event) {
-  if ($('#documentCategorySelector').length) {
-    if ($('#documentCategorySelector').find('option[value=my_home]')[0].selected) {
-      $('#documentGuideSelector').show()
+  if ($(category).length) {
+    if ($(category).find('option[value=my_home]')[0].selected) {
+      $(guide).show()
     } else {
-      $('#documentGuideSelector').hide()
+      $(guide).hide()
+      resetGuide()
     }
   }
 })
+
+// check for multiple documents and selected category
+function multipleFilesCategorySelection () {
+  if ($(category).find('option[value=my_home]')[0].selected &&
+     (($('.single-file-upload').length) || ($('#document_files.file-upload')[0].files.length <= 1))) {
+    $(guide).show()
+  } else {
+    $(guide).hide()
+    resetGuide()
+  }
+}
+
+// reset the guide and dropdown value
+function resetGuide () {
+  $('#documentGuideSelector select')[0].selectedIndex = 0
+  $('#documentGuideSelector .ui-selectmenu-text')[0].innerHTML = "&nbsp;"
+}
