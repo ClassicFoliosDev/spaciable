364ffd792d269048b0f45fffb5a7cf46ab2f1fc5
Robyn <robyn@classicfolios.com>
2020-05-07 09:56:22 +0100

    documents, hide guide dropdown when my home category not selected on page load

--- app/assets/javascripts/documents.js
+++ app/assets/javascripts/documents.js
@@ -1,9 +1,20 @@
-// hide the Guide selector if the Category selector does not have value my_home
+// hide the Guide selector if the Category selector does not have value my_home on selection Category option
 $(document).on('click', '#document_category-menu', function (event) {
   if ($('#documentCategorySelector').find('option[value=my_home]')[0].selected) {
     $('#documentGuideSelector').show()
   } else {
     $('#documentGuideSelector option')[0].selected = true
     $('#documentGuideSelector').hide()
+  }
+})
+
+// hide the Guide selector if the Category selector does not have value my_home on page load
+document.addEventListener('turbolinks:load', function (event) {
+  if ($('#documentCategorySelector').length) {
+    if ($('#documentCategorySelector').find('option[value=my_home]')[0].selected) {
+      $('#documentGuideSelector').show()
+    } else {
+      $('#documentGuideSelector').hide()
+    }
   }
 })
