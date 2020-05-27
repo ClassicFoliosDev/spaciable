747429be2599aa1d0557e342bb0dee01198a521a
Robyn <robyn@classicfolios.com>
2020-05-27 09:54:15 +0100

    cas, finish form text and styling

---
+++ app/assets/javascripts/help.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $("#categoryHelp").hover(function(){
+    $('.category-help').show()
+  }, function() {
+    $('.category-help').hide()
+  })
+
+  $("#typeHelp").hover(function(){
+    $('.type-help').show()
+  }, function() {
+    $('.type-help').hide()
+  })
+
+  $("#manufacturerHelp").hover(function(){
+    $('.manufacturer-help').show()
+  }, function() {
+    $('.manufacturer-help').hide()
+  })
+})
