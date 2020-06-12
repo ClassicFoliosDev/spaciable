268f7f81a6ef988e6c30c8ba60b20bcf3a358555
ClassicFoliosDev <42801626+ClassicFoliosDev@users.noreply.github.com>
2020-06-12 11:12:53 +0100

    488 styling bug (#913)
    * turbolinks remove brand style overrides
    
    * add bad styling links to turbolinks cache ignore
    
    * brand template turbolinks track reload
    
    Co-authored-by: Robyn <robyn@classicfolios.com>

--- app/assets/javascripts/homeowner/plots.js
+++ app/assets/javascripts/homeowner/plots.js
@@ -1,11 +1,7 @@
 (function (document, $) {
   'use strict'
 
-  $(document).on('click', '.my-plots', function (event) {
-    $('.plot-list').toggleClass('hidden')
-  })
-
-  $(document).on('click', '.plot-summary, .branded-logo, .library-component .branded-btn, .my-home .branded-btn, .navbar-item', function (event) {
+  $(document).on('click', '.swap-plot-btn', function (event) {
     $("[data-test='brand-style-overrides']").remove()
   })
 })(document, window.jQuery)
