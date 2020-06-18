2acdc1d1b9202b9cc09f2af4ae049243f4ae0471
Robyn <robyn@classicfolios.com>
2020-06-18 09:58:45 +0100

    rubocop metrics abcsize

--- app/helpers/admin/analytics_helper.rb
+++ app/helpers/admin/analytics_helper.rb
@@ -3,7 +3,8 @@
 module Admin
   module AnalyticsHelper
     def plot_type_collection
-      [%w[Res&Comp all], %w[Reservation res], %w[Completion comp], %w[Created created], %w[Expired expired]]
+      [%w[Res&Comp all], %w[Reservation res], %w[Completion comp],
+       %w[Created created], %w[Expired expired]]
     end
   end
 end
