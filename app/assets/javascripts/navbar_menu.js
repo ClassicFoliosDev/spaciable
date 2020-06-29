f11c5b2cb02b6a9ce1fc03d84bf02001000cc69d
ClassicFoliosDev <42801626+ClassicFoliosDev@users.noreply.github.com>
2020-06-29 08:52:03 +0100

    492 bugs (#923)
    * remove hover background navigation
    
    * dropdown nav clickable area desktop
    
    * dropdown nav styling tablet mobile
    
    * hide unit type text on modal close
    
    * responsive buyers club logo
    
    * banner styling on logout
    
    * refactor navbar js to jquery
    
    * refactor navbar js to jquery
    
    * search bar placeholder text colour
    
    Co-authored-by: Robyn <robyn@classicfolios.com>

--- app/assets/javascripts/navbar_menu.js
+++ app/assets/javascripts/navbar_menu.js
@@ -5,46 +5,10 @@
     if ($('#navList').length) {
       // make the dropdown menu the same width as the name element (desktop only)
       var menuItem = document.getElementById("acctNav")
-      var navList = document.getElementById("navList")
-
-      var menuWidth = window.getComputedStyle(menuItem, null).getPropertyValue("width")
-      navList.style.width = menuWidth
+      $("#navList").css("width", window.getComputedStyle(menuItem, null).width)
 
       // hide the plot address (desktop only)
-      var address = document.getElementById("hiddenAddress")
-      address.style.display = "none"
-    }
-  })
-
-  // toggle the dropdown menu open and closed
-  $(document).on('click', '#acctNav', function (event) {
-    $('#navList').toggle()
-    // toggle the fa arrow up or down depending on if the dropdown menu is open or closed
-    var dropdown = document.getElementById("dropdownMenu")
-    dropdown.classList.toggle("active")
-    var arrow = document.getElementById("upDownArrow")
-    if (dropdown.classList.contains("active")) {
-      arrow.classList.add("fa-angle-up")
-      arrow.classList.remove("fa-angle-down")
-    } else {
-      arrow.classList.add("fa-angle-down")
-      arrow.classList.remove("fa-angle-up")
-    }
-  })
-
-  // change the dropdown menu for mobile view
-  document.addEventListener('turbolinks:load', function () {
-    if ($(window).innerWidth() < 1025) {
-      // change the account navigation icon
-      var arrow = document.getElementById("upDownArrow")
-      arrow.classList = "fa fa-user-circle-o"
-      // set the dropdown width to 100%
-      var navList = document.getElementById("navList")
-      navList.style.width = "100%"
-      // remove header search element
-      var topNav = document.getElementById("topNav")
-      var search = topNav.querySelector(".search-container")
-      search.parentNode.removeChild(search)
+      $("#hiddenAddress").css("display", "none")
     }
   })
 
@@ -55,17 +19,44 @@
     $("body").toggleClass("no-scroll")
   })
 
+  // change the dropdown menu for mobile view
+  document.addEventListener('turbolinks:load', function () {
+    if ($(window).innerWidth() < 1025) {
+      // change the account navigation icon
+      var arrow = document.querySelector("#upDownArrow")
+      arrow.classList = "fa fa-user-circle-o"
+      // set the dropdown width to 100%
+      $("#navList").css("width", "100%")
+      // remove header search element
+      $(".navbar-search .search-container").remove()
+    } else {
+      $(".lower-navigation .lower-navbar-search").remove()
+    }
+  })
+
+  // toggle the dropdown menu open and closed
+  $(document).on('click', '#acctNav', function (event) {
+    $('#navList').toggle()
+    // toggle the fa arrow up or down depending on if the dropdown menu is open or closed
+    $("#dropdownMenu").toggleClass("active")
+    if ($("#dropdownMenu").hasClass("active")) {
+      $("#upDownArrow").addClass("fa-angle-up")
+      $("#upDownArrow").removeClass("fa-angle-down")
+    } else {
+      $("#upDownArrow").removeClass("fa-angle-up")
+      $("#upDownArrow").addClass("fa-angle-down")
+    }
+  })
+
   // mobile view account dropdown alterations
   $(document).on('click', '.dropdown', function (event) {
     if ($(window).innerWidth() < 1025) {
       // hide the logo and hamburger navigation
-      var nav = document.getElementById("topNav")
-      nav.classList.toggle("dropdown-active")
+      $("#topNav").toggleClass("dropdown-active")
       // disable body scrolling
       $("body").toggleClass("no-scroll")
       // show address
-      var address = document.getElementById("hiddenAddress")
-      address.style.display = "block"
+      $("#hiddenAddress").css("display", "block")
       // show sign out arrow
       $('.sign-out-arrow').toggle()
     }
@@ -74,10 +65,9 @@
   // repair styling/functionality when account dropdown closed via click anywhere
   $(document).on('click', '#navList', function (event) {
     // show hamburger navigation
-    var nav = document.getElementById("topNav")
-    nav.classList.remove("dropdown-active")
+    $("#topNav").removeClass("dropdown-active")
     // allow body scrolling
-    document.body.classList.remove("no-scroll")
+    $("body").removeClass("no-scroll")
   })
 
   // open the main navigation
