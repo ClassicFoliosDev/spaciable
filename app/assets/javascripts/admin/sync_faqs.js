1c4713b0766e1b7595dc1456d958ed5efb08e051
Robyn <robyn@classicfolios.com>
2020-07-23 11:31:11 +0100

    sync faqs select all

--- app/assets/javascripts/admin/sync_faqs.js
+++ app/assets/javascripts/admin/sync_faqs.js
@@ -1,4 +1,4 @@
-$(document).on('click', '.select-all', function (event) {
+$(document).on('click', '.category-select-all', function (event) {
   var category = $(this).data().category
   $(".faq_" + category).each(function() {
     var faq = ($(this).find("input"))
@@ -8,9 +8,27 @@ $(document).on('click', '.select-all', function (event) {
   })
 })
 
-$(document).on('click', '.deselect-all', function (event) {
+$(document).on('click', '.category-deselect-all', function (event) {
   var category = $(this).data().category
   $(".faq_" + category).each(function() {
+    var faq = ($(this).find("input"))
+    faq.each(function () {
+      console.log(faq[0].checked = false)
+    })
+  })
+})
+
+$(document).on('click', '.select-all', function (event) {
+  $(".faq-selector").each(function() {
+    var faq = ($(this).find("input"))
+    faq.each(function () {
+      console.log(faq[0].checked = true)
+    })
+  })
+})
+
+$(document).on('click', '.deselect-all', function (event) {
+  $(".faq-selector").each(function() {
     var faq = ($(this).find("input"))
     faq.each(function () {
       console.log(faq[0].checked = false)
