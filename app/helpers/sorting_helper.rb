de11333f511a48eef2850d8b05027c4604bdc820
jason9364 <jason.mardell@gmail.com>
2020-07-31 10:05:45 +0100

    508 FAQ Sorting (#947)
    Co-authored-by: jason.mardell@gmail.com <biker.nut.9364>

--- app/helpers/sorting_helper.rb
+++ app/helpers/sorting_helper.rb
@@ -2,11 +2,13 @@
 
 module SortingHelper
   def sortable(klass, column, title = nil, sort_params: {}, html_options: { class: "both" })
-    return sort_on_association(klass, column, title) if klass.is_a? Hash
+    sort_params[:active_tab] = params[:active_tab]
+    return sort_on_association(klass, column, title, sort_params) if klass.is_a? Hash
 
     title ||= klass.human_attribute_name(column)
 
-    sort_params.merge!(active_tab: params[:active_tab], sort: column, direction: "desc")
+    sort_params[:sort] = column
+    sort_params[:direction] = "desc"
 
     if params[:sort] == column.to_s
       direction = params[:direction] == "asc" ? "desc" : "asc"
@@ -19,10 +21,10 @@ module SortingHelper
     link_to title, sort_params, html_options
   end
 
-  def sort_on_association(hash, column, title)
+  def sort_on_association(hash, column, title, sort_params)
     klass = hash.to_a.last.last
     title ||= klass.model_name.human.to_s
-    sort_params = { sort_on: klass.model_name.param_key }
+    sort_params[:sort_on] = klass.model_name.param_key
 
     sortable(klass, column, title, sort_params: sort_params)
   end
