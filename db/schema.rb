dc3b8726f780fe7755ba63c33536dd8d7e62270e
Robyn <robyn@classicfolios.com>
2020-08-03 12:56:16 +0100

    schema after calendar branch change

--- db/schema.rb
+++ db/schema.rb
@@ -1082,8 +1082,8 @@ ActiveRecord::Schema.define(version: 20200623084553) do
     t.boolean  "receive_choice_emails",     default: false
     t.integer  "lettings_management",       default: 0
     t.boolean  "cas",                       default: false
-    t.boolean  "receive_faq_emails",     default: false
     t.boolean  "receive_invitation_emails", default: true
+    t.boolean  "receive_faq_emails",        default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true, using: :btree
     t.index ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
