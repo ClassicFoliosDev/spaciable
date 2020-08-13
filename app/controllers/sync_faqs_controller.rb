a37206a6a9b7fd30d9fcb784500e802543c8be06
Robyn <robyn@classicfolios.com>
2020-08-13 07:26:19 +0100

    sync faqs basic functionality

--- app/controllers/sync_faqs_controller.rb
+++ app/controllers/sync_faqs_controller.rb
@@ -5,16 +5,25 @@ class SyncFaqsController < ApplicationController
   load_and_authorize_resource :default_faq
 
   def create
-    puts "################################## #{sync_faq_params[:faqs]}"
-    sync_faq_params[:faqs].each do |f|
-      faq = DefaultFaq.find_by(id: f)
-      puts "#################### #{faq}"
+    p_klass = sync_faq_params[:parent_type].classify.constantize
+    parent = p_klass.find_by(id: sync_faq_params[:parent_id])
+
+    sync_faq_params[:faqs].reject!(&:empty?).each do |f|
+      SyncFaq.sync_parent_faqs(f, parent)
     end
+
+    faq_type = FaqType.find(active_tab)
+    puts "#################### #{faq_type}"
+    redirect_to [parent, :faqs, active_tab: faq_type.id]
   end
 
   private
 
+  def active_tab
+    params.permit(:active_tab)[:active_tab]
+  end
+
   def sync_faq_params
-    params.require(:sync_faq).permit(:parent, :faqs => [])
+    params.require(:sync_faq).permit(:parent_id, :parent_type, faqs: [])
   end
 end
--- app/models/sync_faq.rb
+++ app/models/sync_faq.rb
@@ -4,6 +4,22 @@ class SyncFaq
   include ActiveModel::Model
   include ActiveRecord::AttributeAssignment
 
-  attr_accessor :parent, :faqs
+  attr_accessor :parent_id, :parent_type, :faqs
 
+  def self.sync_parent_faqs(faq_id, parent)
+    faq = DefaultFaq.find_by(id: faq_id)
+
+    # if the faq (title) exists for the parent then update
+    # otherwise create a new faq for the parent
+    if (pfaq = Faq.find_by(faqable: parent, question: faq.question))
+      pfaq.update_attribute(:answer, faq.answer)
+    else
+      Faq.create(question: faq.question,
+                 answer: faq.answer,
+                 category: faq.category,
+                 faqable: parent,
+                 faq_type_id: faq.faq_type_id,
+                 faq_category_id: faq.faq_category_id)
+    end
+  end
 end
