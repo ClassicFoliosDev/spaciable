912763340c33f7409305432725ea26aa5c1ab205
jason.mardell@gmail.com <biker.nut.9364>
2020-09-13 13:11:18 +0100

    497 analytics
    Error - included migration

--- db/migrate/20200911145624_crest_divisions.rb
+++
@@ -1,6 +0,0 @@
-class CrestDivisions < ActiveRecord::Migration[5.0]
-  def up
-    Rake::Task['crest:load'].invoke unless Rails.env.test?
-    Rake::Task['crest:migrate'].invoke unless Rails.env.test?
-  end
-end
