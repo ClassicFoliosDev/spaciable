5f8146de921efcfec6dc39c6236553da87c17688
jason9364 <jason.mardell@gmail.com>
2020-10-19 05:42:34 +0100

    522 Allocate Timelines (#969)
    Co-authored-by: jason.mardell@gmail.com <biker.nut.9364>
    Co-authored-by: ClassicFoliosDev <42801626+ClassicFoliosDev@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200911145624) do
+ActiveRecord::Schema.define(version: 20200921094247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -197,6 +197,13 @@ ActiveRecord::Schema.define(version: 20200911145624) do
     t.string   "info_text"
     t.string   "email_logo"
     t.index ["brandable_type", "brandable_id"], name: "index_brands_on_brandable_type_and_brandable_id", using: :btree
+  end
+
+  create_table "cc_emails", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "email_type"
+    t.string  "email_list"
+    t.index ["user_id"], name: "index_cc_emails_on_user_id", using: :btree
   end
 
   create_table "choice_configurations", force: :cascade do |t|
@@ -739,6 +746,13 @@ ActiveRecord::Schema.define(version: 20200911145624) do
     t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
   end
 
+  create_table "phase_timelines", force: :cascade do |t|
+    t.integer "timeline_id"
+    t.integer "phase_id"
+    t.index ["phase_id"], name: "index_phase_timelines_on_phase_id", using: :btree
+    t.index ["timeline_id"], name: "index_phase_timelines_on_timeline_id", using: :btree
+  end
+
   create_table "phases", force: :cascade do |t|
     t.string   "name"
     t.integer  "development_id"
@@ -776,13 +790,13 @@ ActiveRecord::Schema.define(version: 20200911145624) do
   end
 
   create_table "plot_timelines", force: :cascade do |t|
-    t.integer "timeline_id"
+    t.integer "phase_timeline_id"
     t.integer "plot_id"
     t.integer "task_id"
-    t.boolean "complete",    default: false
+    t.boolean "complete",          default: false
+    t.index ["phase_timeline_id"], name: "index_plot_timelines_on_phase_timeline_id", using: :btree
     t.index ["plot_id"], name: "index_plot_timelines_on_plot_id", using: :btree
     t.index ["task_id"], name: "index_plot_timelines_on_task_id", using: :btree
-    t.index ["timeline_id"], name: "index_plot_timelines_on_timeline_id", using: :btree
   end
 
   create_table "plots", force: :cascade do |t|
@@ -1177,6 +1191,7 @@ ActiveRecord::Schema.define(version: 20200911145624) do
   add_foreign_key "appliances", "appliance_manufacturers"
   add_foreign_key "appliances", "developers"
   add_foreign_key "branded_perks", "developers"
+  add_foreign_key "cc_emails", "users"
   add_foreign_key "crms", "developers"
   add_foreign_key "custom_tiles", "developments"
   add_foreign_key "custom_tiles", "documents"
@@ -1205,15 +1220,17 @@ ActiveRecord::Schema.define(version: 20200911145624) do
   add_foreign_key "finishes", "finish_types"
   add_foreign_key "how_tos", "how_to_sub_categories"
   add_foreign_key "maintenances", "developments"
+  add_foreign_key "phase_timelines", "phases"
+  add_foreign_key "phase_timelines", "timelines"
   add_foreign_key "phases", "developers"
   add_foreign_key "phases", "developments"
   add_foreign_key "phases", "divisions"
   add_foreign_key "plot_residencies", "plots"
   add_foreign_key "plot_residencies", "residents"
   add_foreign_key "plot_residencies", "tasks"
+  add_foreign_key "plot_timelines", "phase_timelines"
   add_foreign_key "plot_timelines", "plots"
   add_foreign_key "plot_timelines", "tasks"
-  add_foreign_key "plot_timelines", "timelines"
   add_foreign_key "plots", "developers"
   add_foreign_key "plots", "developments"
   add_foreign_key "plots", "divisions"
