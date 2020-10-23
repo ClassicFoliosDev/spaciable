32a815e93e01ef2dff5094f3d43869f982e1a1a0
Robyn <robyn@classicfolios.com>
2020-10-23 11:19:45 +0100

    styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201013143437) do
+ActiveRecord::Schema.define(version: 20201022084024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -201,6 +201,13 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.index ["brandable_type", "brandable_id"], name: "index_brands_on_brandable_type_and_brandable_id", using: :btree
   end
 
+  create_table "cc_emails", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "email_type"
+    t.string  "email_list"
+    t.index ["user_id"], name: "index_cc_emails_on_user_id", using: :btree
+  end
+
   create_table "choice_configurations", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at",                     null: false
@@ -356,15 +363,15 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.text     "about"
     t.string   "api_key"
     t.string   "list_id"
-    t.integer  "country_id",                                  null: false
     t.boolean  "house_search"
     t.boolean  "enable_services",             default: false
     t.boolean  "enable_development_messages", default: false
     t.boolean  "development_faqs",            default: false
     t.boolean  "enable_roomsketcher",         default: true
+    t.integer  "country_id",                                  null: false
     t.boolean  "enable_referrals",            default: false
-    t.boolean  "cas",                         default: false
     t.boolean  "enable_perks",                default: false
+    t.boolean  "cas",                         default: false
     t.boolean  "timeline",                    default: false
     t.index ["company_name"], name: "index_developers_on_company_name", unique: true, where: "(deleted_at IS NULL)", using: :btree
     t.index ["deleted_at"], name: "index_developers_on_deleted_at", using: :btree
@@ -394,14 +401,14 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.datetime "deleted_at"
     t.integer  "phases_count",          default: 0
     t.string   "segment_id"
-    t.integer  "choice_option",         default: 0,          null: false
-    t.string   "choices_email_contact"
     t.boolean  "enable_snagging",       default: false
     t.integer  "snag_duration",         default: 0
     t.string   "snag_name",             default: "Snagging", null: false
-    t.boolean  "cas",                   default: false
+    t.integer  "choice_option",         default: 0,          null: false
+    t.string   "choices_email_contact"
     t.integer  "construction",          default: 0,          null: false
     t.string   "construction_name"
+    t.boolean  "cas",                   default: false
     t.integer  "timeline_id"
     t.boolean  "calendar",              default: false
     t.index ["deleted_at"], name: "index_developments_on_deleted_at", using: :btree
@@ -438,9 +445,9 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.string   "file"
     t.string   "original_filename"
     t.integer  "category"
-    t.boolean  "pinned",            default: false
     t.string   "file_tmp"
     t.integer  "user_id"
+    t.boolean  "pinned",            default: false
     t.integer  "guide"
     t.index "lower((title)::text) varchar_pattern_ops", name: "search_index_on_document_title", using: :btree
     t.index ["documentable_type", "documentable_id"], name: "index_documents_on_documentable_type_and_documentable_id", using: :btree
@@ -462,7 +469,7 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.string   "resourceable_type"
     t.integer  "resourceable_id"
     t.integer  "status"
-    t.datetime "status_updated_at"
+    t.datetime "status_updated_at", default: -> { "now()" }
     t.datetime "proposed_start"
     t.datetime "proposed_end"
     t.index ["event_id"], name: "index_event_resources_on_event_id", using: :btree
@@ -642,8 +649,8 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.string   "url"
     t.string   "additional_text"
     t.integer  "how_to_sub_category_id"
-    t.integer  "country_id",                             null: false
     t.boolean  "hide",                   default: false
+    t.integer  "country_id",                             null: false
     t.index "lower(summary) varchar_pattern_ops", name: "search_index_on_how_to_summary", using: :btree
     t.index "lower(title) varchar_pattern_ops", name: "search_index_on_how_to_title", using: :btree
     t.index ["how_to_sub_category_id"], name: "index_how_tos_on_how_to_sub_category_id", using: :btree
@@ -811,10 +818,10 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.date     "reservation_release_date"
     t.integer  "validity",                 default: 27
     t.integer  "extended_access",          default: 0
-    t.integer  "choice_configuration_id"
-    t.integer  "choice_selection_status",  default: 0,  null: false
     t.integer  "total_snags",              default: 0
     t.integer  "unresolved_snags",         default: 0
+    t.integer  "choice_configuration_id"
+    t.integer  "choice_selection_status",  default: 0,  null: false
     t.string   "completion_order_number"
     t.string   "reservation_order_number"
     t.string   "uprn"
@@ -1014,10 +1021,10 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.string   "content"
     t.string   "image"
     t.integer  "snag_id"
-    t.string   "commenter_type"
-    t.integer  "commenter_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "commenter_type"
+    t.integer  "commenter_id"
     t.index ["commenter_type", "commenter_id"], name: "index_snag_comments_on_commenter_type_and_commenter_id", using: :btree
     t.index ["snag_id"], name: "index_snag_comments_on_snag_id", using: :btree
   end
@@ -1025,10 +1032,10 @@ ActiveRecord::Schema.define(version: 20201013143437) do
   create_table "snags", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.integer  "status",      default: 0
-    t.integer  "plot_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.integer  "plot_id"
+    t.integer  "status",      default: 0
     t.index ["plot_id"], name: "index_snags_on_plot_id", using: :btree
   end
 
@@ -1154,8 +1161,8 @@ ActiveRecord::Schema.define(version: 20201013143437) do
     t.string   "picture"
     t.string   "job_title"
     t.boolean  "receive_release_emails",    default: true
-    t.boolean  "receive_choice_emails",     default: false
     t.boolean  "snag_notifications",        default: true
+    t.boolean  "receive_choice_emails",     default: false
     t.integer  "lettings_management",       default: 0
     t.boolean  "cas",                       default: false
     t.boolean  "receive_invitation_emails", default: true
@@ -1187,6 +1194,7 @@ ActiveRecord::Schema.define(version: 20201013143437) do
   add_foreign_key "appliances", "appliance_manufacturers"
   add_foreign_key "appliances", "developers"
   add_foreign_key "branded_perks", "developers"
+  add_foreign_key "cc_emails", "users"
   add_foreign_key "crms", "developers"
   add_foreign_key "custom_tiles", "developments"
   add_foreign_key "custom_tiles", "documents"
