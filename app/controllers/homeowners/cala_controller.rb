0ca8e8e87ff5187eb75e8ccabff93d7c130ca997
jason.mardell@gmail.com <biker.nut.9364>
2022-05-01 14:57:36 +0100

    749 Swap to alert

--- app/controllers/homeowners/cala_controller.rb
+++ app/controllers/homeowners/cala_controller.rb
@@ -4,9 +4,9 @@ module Homeowners
   class CalaController < Homeowners::BaseController
     # rubocop:disable LineLength, OutputSafety
     def bt_shop
-      return redirect_to root_path, notice: I18n.t("cala.not_res").html_safe unless current_resident.cala?(params.key?("pemd"))
+      return redirect_to root_path, alert: I18n.t("cala.not_res").html_safe unless current_resident.cala?(params.key?("pemd"))
       error, url = Uniqodo.redeem(current_resident.email)
-      return redirect_to root_path, notice: error if error
+      return redirect_to root_path, alert: error if error
       redirect_to url
     end
   end
