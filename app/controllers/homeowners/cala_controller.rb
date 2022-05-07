9f85f3e0c7f8893c426f0a32d195dd080ca5e12f
jason9364 <jason.mardell@gmail.com>
2022-05-07 13:43:39 +0100

    749 alternative codes (#1182)
    Co-authored-by: jason.mardell@gmail.com <biker.nut.9364>

--- app/controllers/homeowners/cala_controller.rb
+++ app/controllers/homeowners/cala_controller.rb
@@ -5,7 +5,14 @@ module Homeowners
     # rubocop:disable LineLength, OutputSafety
     def bt_shop
       return redirect_to root_path, alert: I18n.t("cala.not_res").html_safe unless current_resident.cala?(params.key?("pemd"))
-      error, url = Uniqodo.redeem(current_resident.email)
+      error, url = Uniqodo.redeem("13482", current_resident.email)
+      return redirect_to root_path, alert: error if error
+      redirect_to url
+    end
+
+    def offers
+      return redirect_to root_path, alert: I18n.t("cala.not_res").html_safe unless current_resident.cala?(params.key?("pemd"))
+      error, url = Uniqodo.redeem(params[:offer_code], current_resident.email)
       return redirect_to root_path, alert: error if error
       redirect_to url
     end
