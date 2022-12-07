d55ad3a79ea0d2e552c1e18eace747199d682bed
jason9364 <jason.mardell@gmail.com>
2022-12-07 08:39:59 +0000

    784 single page app (#1209)
    * 784SinglePageApp
    
    * Pre new spaciable_doorkeeper version integration
    
    * Add CORS configuration
    
    * Rubocop
    
    Co-authored-by: Robyn <robyn@classicfolios.com>
    Co-authored-by: jason.mardell@gmail.com <biker.nut.9364>
    Co-authored-by: ClassicFoliosDev <42801626+ClassicFoliosDev@users.noreply.github.com>

--- app/controllers/api/admin/admin_controller.rb
+++ app/controllers/api/admin/admin_controller.rb
@@ -3,14 +3,27 @@
 module Api
   module Admin
     class AdminController < ActionController::Base
-      before_action -> { doorkeeper_authorize! :admin }
-      before_action :sign_user_in
+      before_action -> { doorkeeper_authorize! :admin }, only: %i[create]
+      before_action :sign_user_in, only: %i[create]
+
+      before_action -> { doorkeeper_authorize! }, only: %i[single_page_app_create]
+      before_action :sign_app_in, only: %i[single_page_app_create]
+
       respond_to :json
 
       private
 
       def sign_user_in
         sign_in(doorkeeper_token.resource)
+        RequestStore.store[:current_user] = current_user
+      end
+
+      def sign_app_in
+        # Yes this looks wrong!  doorkeeper_token.app_user DOES
+        # return a User but it fails to satisfy 'is_a? User' for
+        # some reason.  It looks like a bug cleared in later
+        # versions of Ruby
+        sign_in(User.find(doorkeeper_token.app_user.id))
         RequestStore.store[:current_user] = current_user
       end
     end
--- app/controllers/api/admin/v1/pre_sales_controller.rb
+++ app/controllers/api/admin/v1/pre_sales_controller.rb
@@ -4,16 +4,20 @@ module Api
   module Admin
     module V1
       class PreSalesController < Api::Admin::AdminController
-        def create
-          Api::PreSales.new(pre_sales_params).add_limited_access_user do |m, s, h_s|
-            render json: { message: m, result: s }, status: h_s
-            break
+        METHODS ||= %w[create single_page_app_create].freeze
+
+        METHODS.each do |endpoint|
+          define_method endpoint do
+            Api::PreSales.new(pre_sale_params).add_limited_access_user do |m, s, h_s|
+              render json: { message: m, result: s }, status: h_s
+              break
+            end
           end
         end
 
         private
 
-        def pre_sales_params
+        def pre_sale_params
           params.require(:pre_sale).permit(
             :development, :division, :phase, :plot_number,
             :email, :phone_number, :title, :first_name, :last_name
