5c77753ec6192a9018b264c7a1d621ed46a7fa8b
Robyn <robyn@classicfolios.com>
2022-12-20 17:04:43 +0000

    789 Add Prospect

--- app/helpers/plot_residencies_helper.rb
+++ app/helpers/plot_residencies_helper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 module PlotResidenciesHelper
-  def roles_collection
+  def roles_collection(full = false)
+    roles = []
     PlotResidency.roles.map do |(role_name, _role_int)|
-      [t(role_name, scope: role_scope), role_name]
+      next if role_name == "prospect" && !full
+      roles << [t(role_name, scope: role_scope), role_name]
     end
+
+    roles
   end
 
   private
