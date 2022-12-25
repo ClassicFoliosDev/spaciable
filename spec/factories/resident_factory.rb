65ff68a3d06686b64a3130d598a216eb6968de1c
Robyn <robyn@classicfolios.com>
2022-12-25 13:32:06 +0000

    Missed settings for default role of "homeowner"

--- spec/factories/resident_factory.rb
+++ spec/factories/resident_factory.rb
@@ -6,6 +6,7 @@ FactoryGirl.define do
     password { Faker::Internet.password(8) }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    role { "homeowner" }
     phone_number { "0300 200 3300" }
 
     trait :activated do
