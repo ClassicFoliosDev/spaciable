75ac3d80724908d112ed9741e9a09a64a36845a7
jason9364 <jason.mardell@gmail.com>
2023-01-14 13:41:54 +0000

    792 CORS_ORIGIN (#1219)
    Co-authored-by: Robyn <robyn@classicfolios.com>

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://brilliant-homes.webflow.io'
+    origins ENV.fetch("CORS_ORIGIN","").split(",")
     resource '/oauth/token', headers: :any, methods: [:post]
     resource '/api/admin/single_page_app_pre_sales', headers: :any, methods: [:post]
   end
