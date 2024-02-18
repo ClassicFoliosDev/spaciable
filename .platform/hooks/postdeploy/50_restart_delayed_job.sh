5b7040b42a24f7b282e47f4140b13e52a46686a1
jason mardell <jason.mardell@gmail.com>
2024-02-18 17:07:39 +0000

    respawn delayed job

--- .platform/hooks/postdeploy/50_restart_delayed_job.sh
+++ .platform/hooks/postdeploy/50_restart_delayed_job.sh
@@ -4,4 +4,8 @@ EB_APP_CURRENT_DIR=$(/opt/elasticbeanstalk/bin/get-config platformconfig -k AppD
 cd $EB_APP_CURRENT_DIR
 
 sudo chmod 0666 log/delayed_job.log
-bin/delayed_job --pid-dir=/var/app/containerfiles/pids restart
+
+trap 'kill $(jobs -p)' EXIT; until bin/delayed_job --pid-dir=/var/app/containerfiles/pids restart & wait; do
+    echo "delayed_job crashed with exit code $?. Respawning.." >&2
+    sleep 1
+done
