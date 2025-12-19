ef522355cd57ee4b48837415f4c2b4e5d934ac2a
jason mardell <jason.mardell@gmail.com>
2025-12-19 17:48:43 +0000

    12

--- .cloudbees/scripts/stream_settings.sh
+++ .cloudbees/scripts/stream_settings.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-echo $BRANCH
+dt=$(date +'%Y-%m-%d %H:%M')
 
-export ZIP="CB_staging.zip"
-export ENVIRONMENT="Staging-27-env"
+if [[ $BRANCH == master* ]] 
+then
+  export ENVIRONMENT="production-environment"
+  export ZIP="CB_master_$dt.zip"
+
+else
+  export ENVIRONMENT="Staging-27-env"
+  export ZIP="CB_staging_$dt.zip"
+fi
