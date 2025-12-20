3d445b272d2e501aaad9054ebd302b9982e8d2a3
jason mardell <jason.mardell@gmail.com>
2025-12-20 08:52:07 +0000

    15

--- .cloudbees/scripts/stream_settings.sh
+++ .cloudbees/scripts/stream_settings.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo $CI_BRANCH
+
 dt=$(date +'%Y-%m-%d %H:%M')
 
-if [[ $BRANCH == master* ]] 
+if [[ $BRANCH == release* ]] 
 then
   export ENVIRONMENT="production-environment"
   export ZIP="CB_master_$dt.zip"
