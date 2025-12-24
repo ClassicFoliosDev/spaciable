e0728db6153357f32080e5c7563041768b69c6c6
jason mardell <jason.mardell@gmail.com>
2025-12-24 15:42:50 +0000

    zipfile

--- .cloudbees/scripts/stream_settings.sh
+++ .cloudbees/scripts/stream_settings.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-dt=$(date +'%Y%m%d%H%M%S')
-
 if [[ $CI_BRANCH == release* ]] 
 then
   export ENVIRONMENT="production-environment"
-  export ZIPFILE="CB_master_$dt.zip"
   export APPLICATION_NAME="release-27"
 else
   export ENVIRONMENT="Staging-27-env"
-  export ZIPFILE="CB_staging_$dt.zip"
   export APPLICATION_NAME="staging-27"
 fi
-
-export ZIPFILE="CB_staging_20251220172604.zip"
